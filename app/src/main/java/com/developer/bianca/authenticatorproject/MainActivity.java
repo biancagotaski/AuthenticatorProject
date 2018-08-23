5fdf2676c454749dcf5fe8156eabf13f26fa04d0
Bianca Gotaski <biancagotaski@outlook.com>
2018-08-23 17:25:10 -0300

    First commit

---
+++ app/src/main/java/com/developer/bianca/authenticatorproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.developer.bianca.authenticatorproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+}
